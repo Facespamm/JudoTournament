67aef9bf0ab4898141796dd4baa90b4a6a7ab0b2
Facespamm <turrin.shura@gmail.com>
2025-11-11 15:37:23 +0500

    Add subscription form to App and Login components
    Introduced a form for name and email subscription in both App.vue and Login.vue, replacing placeholder content in Login.vue. Also updated auth-background styling to ensure it stays behind form content.

--- JudoTournamentWeb/src/App.vue
+++ JudoTournamentWeb/src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="auth-background"></div>
+  <form action="" method="get" class="form-example">
+    <div class="form-example">
+      <label for="name">Enter your name: </label>
+      <input type="text" name="name" id="name" required />
+    </div>
+    <div class="form-example">
+      <label for="email">Enter your email: </label>
+      <input type="email" name="email" id="email" required />
+    </div>
+    <div class="form-example">
+      <input type="submit" value="Subscribe!" />
+    </div>
+  </form>
+
 
 </template>
 
@@ -17,5 +31,5 @@
   position: fixed;
   top: 0;
   left: 0;
-}
-</style>
+  z-index: -1; /* Помещает фон под содержимое */
+}</style>
--- JudoTournamentWeb/src/components/View/Authentication/Login/Login.vue
+++ JudoTournamentWeb/src/components/View/Authentication/Login/Login.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="container">
-    <h1>Заголовок</h1>
-    <p>Текст внутри контейнера</p>
-  </div>
+  <form action="" method="get" class="form-example">
+    <div class="form-example">
+      <label for="name">Enter your name: </label>
+      <input type="text" name="name" id="name" required />
+    </div>
+    <div class="form-example">
+      <label for="email">Enter your email: </label>
+      <input type="email" name="email" id="email" required />
+    </div>
+    <div class="form-example">
+      <input type="submit" value="Subscribe!" />
+    </div>
+  </form>
 
 </template>
 
